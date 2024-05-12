12e4496de4767f6c0044f5abb3898593892a1d07
Sara <borrasara03@gmail.com>
2024-05-12 15:56:22 +0200

    Initial commit

---
+++ src/main/java/controller/MonthController.java
@@ -0,0 +1,18 @@
+package controller;
+
+import entity.Month;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/month")
+public class MonthController {
+
+    @GetMapping("")
+    public Month getMonth(HttpServletRequest request){
+        Month month = (Month) request.getAttribute("MonthInterceptor-month");
+        return month;
+    }
+}
---
+++ src/main/java/entity/Month.java
@@ -0,0 +1,54 @@
+package entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Month {
+
+    private int monthNumber;
+    private String englishName;
+    private String italianName;
+    private String spanishName;
+
+
+    //getter setter e costruttore a mano perchè nel mio ide ancora non va l'annotation data(solo quella per qualche motivo)
+    public Month(int i, String january, String gennaio, String enero) {
+    }
+
+    public int getMonthNumber() {
+        return monthNumber;
+    }
+
+    public void setMonthNumber(int monthNumber) {
+        this.monthNumber = monthNumber;
+    }
+
+    public String getEnglishName() {
+        return englishName;
+    }
+
+    public void setEnglishName(String englishName) {
+        this.englishName = englishName;
+    }
+
+    public String getItalianName() {
+        return italianName;
+    }
+
+    public void setItalianName(String italianName) {
+        this.italianName = italianName;
+    }
+
+    public String getSpanishName() {
+        return spanishName;
+    }
+
+    public void setSpanishName(String spanishName) {
+        this.spanishName = spanishName;
+    }
+    
+}
